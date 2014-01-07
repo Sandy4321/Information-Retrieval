104594e1f17abb9e4b6152cd90d5b22898b63355
Madan Ram <madan_ram@rocketmail.com>
2014-01-07 17:45:42 +0530

    Update main.py

--- test/main.py
+++ test/main.py
@@ -61,11 +61,10 @@ def processData(data,disableStopWords=True):
 			ListOfWord=[]
 			for i in xrange(len(dataToken)):
 				ListOfWord.append(stopWords(dataToken[i]))
-
-			for i in xrange(len(ListOfWord)):
-				stemWord(ListOfWord[i])
 		else:
 			ListOfWord=dataToken
+	for i in xrange(len(ListOfWord)):
+		stemWord(ListOfWord[i])
 	return ListOfWord
 
 def score(ListOfQuery,numberOfDoc,ListOfIndexDoc,iteamWithPos):
@@ -109,32 +108,3 @@ def getWords():
 		print p+1
 
 getWords()
-
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-#import sys
-#N = int(raw_input())
-#data = sys.stdin.readlines()
-
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#vectorizer = TfidfVectorizer(min_df=1)
-#tfidf = vectorizer.fit_transform(data)
-#sim = (tfidf * tfidf.T)
-
-#usedA = set()
-#usedB = set()
-#solution = dict()
-
-#vals = [(-sim[i,N+1+j], i, j) for i in range(N) for j in range(N)]
-
-#for v,i,j in sorted(vals):
-    #print v,i,j
-#    if i in usedA or j in usedB: continue
-#   usedA.add(i)
-#    usedB.add(j)
-#    solution[i]=j
-
-#for i in range(N):
-#    print solution[i]+1
-#    print solution[i]+1
-#    print solution[i]+1
